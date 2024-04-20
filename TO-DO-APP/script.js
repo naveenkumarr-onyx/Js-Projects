92633142669e8d05b349ed69e30b28fabd8e2865
Naveenkumar <121660045+naveenkumarr-onyx@users.noreply.github.com>
2024-04-20 23:16:11 +0530

    added todo-list

--- TO-DO-APP/script.js
+++ TO-DO-APP/script.js
@@ -1,32 +1,59 @@
+// Create a "close" button and append it to each list item
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function () {
+    var div = this.parentElement;
+    div.style.display = "none";
+  };
+}
+
+// Add a "checked" symbol when clicking on a list item
+var list = document.querySelector("ul");
+list.addEventListener(
+  "click",
+  function (ev) {
+    if (ev.target.tagName === "LI") {
+      ev.target.classList.toggle("checked");
+    }
+  },
+  false
+);
+
+// Create a new list item when clicking on the "Add" button
 function newElement() {
   var li = document.createElement("li");
   var inputValue = document.getElementById("inputValue").value;
   var t = document.createTextNode(inputValue);
   li.appendChild(t);
-  if (!inputValue) {
-    alert("Please enter something");
+  if (inputValue === "") {
+    alert("You must write something!");
   } else {
     document.getElementById("myUL").appendChild(li);
   }
-  var span = document.createElement("span");
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
   var txt = document.createTextNode("\u00D7");
   span.className = "close";
   span.appendChild(txt);
+  li.appendChild(span);
 
   for (i = 0; i < close.length; i++) {
-    close[i].onClick = () => {
+    close[i].onclick = function () {
       var div = this.parentElement;
       div.style.display = "none";
     };
   }
 }
-
-// var myNodeList = document.getElementsByTagName("li");
-// var i;
-// for (i = 0; i < myNodeList.length; i++) {
-//   var span = document.createElement("span");
-//   var txt = document.createTextNode("\u00D7");
-//   span.className = "close";
-//   span.appendChild(txt);
-//   myNodeList[i].appendChild(span);
-// }
