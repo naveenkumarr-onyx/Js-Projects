cca10c8d88483687410805304143f8737be1bfd7
Naveenkumar <121660045+naveenkumarr-onyx@users.noreply.github.com>
2024-04-20 23:46:25 +0530

    added filter

---
+++ Filter-search/index.js
@@ -0,0 +1,20 @@
+function search() {
+  myFunction();
+}
+function myFunction() {
+  var input, filter, ul, li, a, textValue;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("myUL");
+  li = ul.getElementsByTagName("li");
+
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("a")[0];
+    textValue = a.textContent || a.innerText;
+    if (textValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
