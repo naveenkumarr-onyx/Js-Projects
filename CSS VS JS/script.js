91afee66f007617247edb5f7dbdb44f924a4a440
Naveenkumar <121660045+naveenkumarr-onyx@users.noreply.github.com>
2024-04-26 22:12:53 +0530

    updated

--- CSS VS JS/script.js
+++ CSS VS JS/script.js
@@ -1,13 +1,17 @@
-let range_input = document.querySelector("input");
-// range_input.addEventListener("drag", () => {
-//   console.log("Draging");
-// });
-range_input.addEventListener("change", handleChange);
-function handleChange() {
-  const suffix = this.dataset.sizing || "";
-  document.documentElement.style.setProperty(
-    `--${this.name}`,
-    this.value + suffix
-  );
-  console.log(suffix);
-}
+let range_input = document.querySelectorAll(".main-container input");
+range_input.forEach((input) => input.addEventListener("change", handleChange));
+function handleChange() {}
+// range_input.addEventListener("change", handleChange);
+// function handleChange() {
+//   const suffix = this.dataset.sizing || "";
+//   document.documentElement.style.setProperty(
+//     `--${this.name}`,
+//     this.value + suffix
+//   );
+//   console.log(suffix);
+// }
+// function handleChange() {
+//   // var a = document.getElementsByTagName("img");
+//   var b = 10;
+//   var a = document.getElementsById("img-ctn");
+// }
